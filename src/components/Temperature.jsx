c481a24e795a55e7196c93bde96587a0c4e46714
shoeb240 <shoeb240@gmail.com>
2023-08-15 17:27:21 +0600

    Dynamic label

--- src/components/Temperature.jsx
+++ src/components/Temperature.jsx
@@ -2,7 +2,7 @@ export default function temperature({unit, temperature, myChange}) {
     console.log(temperature);
     return (
         <fieldset>
-            <legend>Pharenheit</legend>
+            <legend>{unit.toUpperCase()}</legend>
             <input type="text" value={temperature} onChange={(e) => myChange(e, unit)} />
         </fieldset>
     );
